547e5dd13a861ba1610a1d5b158d523772724ed2
pawli <dbit.kontakt@gmail.com>
2024-11-07 21:21:55 +0100

    ✨ Checking permissions for client, and send normal message when client don't have permissions

--- src/function.js
+++ src/function.js
@@ -1,96 +1,93 @@
-import Discord from 'discord.js'
+import Discord from 'discord.js';
 import Utils from './utils.js';
 
-class fastHook {
+/**
+ * @typedef {Object} SendOptions
+ * @property {string|null} [webhookName] - Webhook name in channel integrations tab
+ * @property {string|"No username provided"} [username] - Username for webhook
+ * @property {string|null} [icon] - Avatar for webhook ( must be URL to image )
+ * @property {string|"Fast-hook module for discord bots"} [reason] - Reason for creating webhook in provided channel
+ * @property {string|"No content provided"} [content] - Message content for webhook
+ * @property {Array|null} [embeds] - Array with embeds to send ( Check readme.md for more info )
+ * @property {Array|null} [files] - Array with files to send ( Check readme.md for more info )
+ * @property {Array|null} [components] - Array with files to send ( Check readme.md for more info )
+ * @property {boolean} [mentions] - Allow mentions in webhook message
+ * @property {boolean} [silent] - Send message in silent mode
+ * @property {Object|null} [poll] - Object with poll to send ( Check readme.md for more info )
+ */
 
-    static async send(trigger, options = {
-        webhookName: null,
-        username: "No username provided",
-        icon: null,
-        reason: "Fast-hook for bot",
+class FastHook {
 
-        content: "No content provided",
-        embeds: null,
-        files: null,
-        components: null,
-        mentions: false,
-        silent: false,
-        flags: [],
-        poll: null
-    }) {
+    /**
+     * Sends a message via a webhook quickly.
+     *
+     * @param {Discord.TextChannel | string} trigger - Text channel or Webhook URL to send message to.
+     * @param {SendOptions} [options={ webhookName = null, username = "No username provided", icon = null, reason = "Fast-hook module for discord bots", content = "No content provided", embeds = null, files = null, components = null, mentions = false, silent = false, poll = null }] - Options for webhook message.
+     * @returns {Promise<Discord.Message|undefined>}
+     */
+    static async send(trigger, options = {}) {
+        const {
+            webhookName = null,
+            username = "No username provided",
+            icon = null,
+            reason = "Fast-hook module for discord bots",
+            content = "No content provided",
+            embeds = null,
+            files = null,
+            components = null,
+            mentions = false,
+            silent = false,
+            flags = [],
+            poll = null
+        } = options;
 
         try {
-
             let hook;
 
-            if(!trigger)
-                return new TypeError("[FAST-HOOK] You must provide trigger (GuildChannel or Webhook URL)")
-
-            else {
-
-                if(Utils.checkIfWebhook(trigger)) {
-
-                    const webhookData = Utils.extractWebhookData(trigger)
-
-                    hook = new Discord.WebhookClient({
-                        id: webhookData.id,
-                        token: webhookData.token
-                    }) || undefined;
-
-                } else if(!trigger.send && !trigger.fetchWebhooks())
-                    return new TypeError("[FAST-HOOK] You must provide trigger (GuildChannel or Webhook URL)")
-                else if(!hook) {
-                    await trigger.fetchWebhooks()
-                        .then((hooks) => {
-                            if (!hooks)
-                                return;
-
-                            hook = hooks.find(hook => hook.owner.id === trigger.client.user.id) || undefined
-                        })
-                        .catch((err) => {
-                            return new TypeError("[FAST-HOOK] Failed to fetch/find webhooks from/in provided channel")
-                        })
-                }
-                if(!hook && trigger.send) {
-
-                    hook = await trigger.createWebhook({
-                        name: options.webhookName || trigger.client.user.username,
-                        avatar: options.icon || trigger.client.user.displayAvatarURL(),
-                        reason: options.reason
-                    }).catch((err) => {
-                        return new TypeError(`[FAST-HOOK] I don't have permissions to create/manage webhooks; #${trigger.name} (${trigger.id}) at guild ${trigger.guild.name} (${trigger.guild.id})`)
-                    });
-                } else if(!hook && !trigger)
-                    return new TypeError("[FAST-HOOK] Invalid method to send webhook. Please provide trigger (GuildChannel or Webhook URL)")
+            if (!trigger) {
+                throw new TypeError("[FAST-HOOK] You must provide a valid trigger (GuildChannel or Webhook URL)");
             }
 
-            if(hook) {
+            const messagePayload = {
+                content,
+                embeds,
+                files,
+                components,
+                poll,
+                flags: silent ? [...flags, 4096] : flags,
+                allowedMentions: mentions ? undefined : { parse: [] }
+            };
 
-                const messagePayload = {
-                    username: options.username,
-                    avatarURL: options.icon,
-
-                    content: options.content,
-                    embeds: options.embeds,
-                    files: options.files,
-                    components: options.components,
-                    flags: options.flags || [],
-                    poll: options.poll,
+            if (Utils.checkIfWebhook(trigger)) {
+                const { id, token } = Utils.extractWebhookData(trigger);
+                hook = new Discord.WebhookClient({ id, token });
+            } else {
+                const permissions = trigger.guild.members.me.permissionsIn(trigger);
+                if (!permissions.has("ManageWebhooks")) {
+                    if (permissions.has("SendMessages")) {
+                        return await trigger.send(messagePayload);
+                    } else {
+                        throw new TypeError("[FAST-HOOK] Missing permissions to send messages or manage webhooks.");
+                    }
                 }
 
-                if(!options.mentions)
-                    messagePayload.allowedMentions = { parse: [] }
-
-                if(options.silent)
-                    messagePayload.flags.push(4096);
-
-                return await hook.send(messagePayload);
+                hook = await Utils.getOrCreateHook(trigger, webhookName, icon, reason);
             }
 
-        } catch (e) {
-            throw new TypeError(`[FAST-HOOK] Something went wrong (Probably something in message content must same like in docs - https://discord.js.org/docs/packages/discord.js/14.16.3/MessageCreateOptions:Interface) ${e}`);
+            if (!hook) {
+                throw new TypeError("[FAST-HOOK] Failed to initialize webhook. Check permissions and channel or webhook url setup.");
+            }
+
+            Object.assign(messagePayload, {
+                username,
+                avatarURL: icon
+            });
+
+            return await hook.send(messagePayload);
+        } catch (error) {
+            throw new TypeError(`[FAST-HOOK] An error occurred while sending the webhook message: ${error.message}`);
         }
     }
 }
 
-export default fastHook;
+export default FastHook;
