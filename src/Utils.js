1649127e45c0756bb92d847d7a04a0770b1ee107
pawli <dbit.kontakt@gmail.com>
2024-11-07 23:12:51 +0100

    ✨ New version 2.1.0

--- src/Utils.js
+++ src/Utils.js
@@ -1,5 +1,18 @@
-
 class Utils {
+    static async getOrCreateHook(channel, name, icon, reason) {
+        if (!channel.send || !channel.fetchWebhooks) {
+            throw new TypeError("[FAST-HOOK] Invalid trigger provided. Must be a GuildChannel or Webhook URL.");
+        }
+
+        const webhooks = await channel.fetchWebhooks().catch(() => null);
+        if (webhooks) {
+            const existingHook = webhooks.find(h => h.owner.id === channel.client.user.id);
+            if (existingHook) return existingHook;
+        }
+
+        return channel.createWebhook({ name, avatar: icon, reason }).catch(() => null);
+    }
+
     static checkIfWebhook(link) {
         const regex = /^https:\/\/(?:canary\.|ptb\.)?discord\.com\/api\/webhooks\/\d+\/[\w-]+$/;
         return regex.test(link);
